f67d6b99f0966236d65fabfca680483aed91b1c3
Trey Krizek <treyvin03@gmail.com>
2025-08-28 11:51:32 -0500

    Completed hw0

--- hw0.py
+++ hw0.py
@@ -1,3 +1,5 @@
+import math
+import numpy as np
 from rootSolvers import *
 """
 Workspace for hw0
@@ -13,12 +15,12 @@ args = (0.6, 1.4)
 root1a = rootSolvers.secant_solver(f1_res, *args, ic = 0.5)
 root1b = rootSolvers.secant_solver(f1_res, *args, ic = 1.5)
 print("Problem 1 (Secant Method)")
-print(f"1a. {root1a:.5f}; 1b. {root1b:.5f}\n")
+print(f"1a. {root1a}; 1b. {root1b}\n")
 
 root1a = rootSolvers.bisection_solver(f1_res, *args, ivl = [0,1])
 root1b = rootSolvers.bisection_solver(f1_res, *args, ivl = [1,10])
 print("Problem 1 (Bisection Method)")
-print(f"1a. {root1a:.5f}; 1b. {root1b:.5f}\n")
+print(f"1a. {root1a}; 1b. {root1b}\n")
 
 # Problem 2
 
@@ -28,9 +30,30 @@ def f2_res(sp, ast_by_a, g):
 root2a = rootSolvers.secant_solver(f2_res, *args, ic = 1.5)
 root2b = rootSolvers.secant_solver(f2_res, *args, ic = 2)
 print("Problem 2 (Secant Method)")
-print(f"2a. {root2a:.5f}; 2b. {root2b:.5f}\n")
+print(f"2a. {root2a}; 2b. {root2b}\n")
 
 root2a = rootSolvers.bisection_solver(f2_res, *args, ivl = [1, 1.893])
 root2b = rootSolvers.bisection_solver(f2_res, *args, ivl = [1.893, 10])
 print("Problem 2 (Bisection Method)")
-print(f"2a. {root2a:.5f}; 2b. {root2b:.5f}\n")
+print(f"2a. {root2a}; 2b. {root2b}\n")
+
+# Problem 3
+
+def f3_res(t, b, M1, g):
+    lhs = np.tan(b) / np.tan(b - t)
+    rhs = ((g+1) * M1**2 * np.sin(b)**2) / (2 + ((g-1) * M1**2 * np.sin(b)**2))
+    return lhs - rhs
+
+beta_a = np.deg2rad(30.935)
+
+args3 = (beta_a, 4.5, 1.4)
+root3a = np.rad2deg(rootSolvers.secant_solver(f3_res, *args3, ic = np.deg2rad(20)))
+print("Problem 3 (Bisection Method)")
+print(f"3a. {root3a}; ")
+
+beta_a = np.deg2rad(84.269)
+
+args3 = (beta_a, 4.5, 1.4)
+root3b = np.rad2deg(rootSolvers.secant_solver(f3_res, *args3, ic = np.deg2rad(45)))
+print(f"3b. {root3b}")
+
